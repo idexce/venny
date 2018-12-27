29e20c98fdee5ea2267d1f976f2a6ccfde0544ef
idexcel-interns <46163763+idexcel-interns@users.noreply.github.com>
2018-12-27 15:43:39 -0500

    updated index.js

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/node', function (req, res) {
   res.send('Hello....!!!!!')
 })
 
+app.get('/application', function (req, res) {
+  res.send('Hello app....!!!!!')
+})
+
 //Launch listening server on port 8081
 app.listen(8081, function () {
   console.log('app listening on port 8081!')
